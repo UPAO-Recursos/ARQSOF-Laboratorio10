05248edcd8520bf1b122965af728b2a2151fa104
Walter Leturia <leturia@live.com>
2024-06-19 01:54:52 +0000

    var instead of env

--- iac/main.tf
+++ iac/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region      = "us-east-2"
-  access_key  = env.aws_access_key
-  secret_key  = env.aws_secret_key
+  access_key  = var.aws_access_key
+  secret_key  = var.aws_secret_key
   max_retries = 2
 
   default_tags {
